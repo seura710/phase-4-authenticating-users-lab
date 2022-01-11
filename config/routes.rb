5b7f35c28701402e6122239a158792a017dae89b
Harris Kim <seura710@gmail.com>
2022-01-11 12:10:00 -0500

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #session routes for login / logout
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  #user route
+  get '/me', to: 'users#show'
+  post '/signup', to: 'users#create'
 end
